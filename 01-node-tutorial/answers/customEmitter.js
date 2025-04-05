5585842ec53e700a3236cf747161be3c3825202d
John Gutierrez <75353216+canilo1@users.noreply.github.com>
2025-04-05 13:21:46 -0700

    CustomEmitter.js

---
+++ 01-node-tutorial/answers/customEmitter.js
@@ -0,0 +1,6 @@
+const EventEmitter = require("events");  
+const emitter = new EventEmitter();  
+setInterval(() => {  
+  emitter.emit("timer", "hi there");  
+}, 2000);  
+emitter.on("timer", (msg) => console.log(msg));  
